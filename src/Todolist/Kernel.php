256356dfbe70e89c2dc7cc06b4b1dfc7d1cd6f35
Gerlovsky Ruslan <Gerlovsky.Ruslan@gmail.com>
2019-01-15 15:45:39 +0500

    Change main src

--- src/Todolist/Kernel.php
+++ src/Todolist/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Todolist;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -15,9 +15,20 @@ class Kernel extends BaseKernel
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
+    public function getCacheDir()
+    {
+        return $this->getProjectDir().'/var/cache/'.$this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return $this->getProjectDir().'/var/log';
+    }
+
     public function registerBundles()
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
+
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
